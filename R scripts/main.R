e43a51b776e95373682f46e99a766ab20c671817
James Hawkins <jameshawkins702@gmail.com>
2023-10-30 14:44:23 +0100

    Some commit

--- R scripts/main.R
+++ R scripts/main.R
@@ -1,40 +1,34 @@
-getwd()
+
+
+getwd()   # Check what directory we are currently in
 
 
 # Insert paths to data and script directories here, with folders separated by \\ (double backward slash)
-data.dir <- ('')
-script.dir <- ('')
+data.dir <- ('C:\\Users\\hawkj\\Documents\\Github\\LS Fall 23\\Practicals\\Download\\\\Data\\')
+script.dir <- ('C:\\Users\\hawkj\\Documents\\Github\\LS Fall 23\\Practicals\\Download\\\\R scripts\\')
 
   
 
-
+# Specify script directory as the main directory to start the code from
 setwd(script.dir)
 
 
 
-source('glob.funcs.R')
+source('glob.funcs.R') # Load the necessary packages/functions needed for the analysis
 
 
+# Define function that processes our entire survey transformation script
 gen.data <- function(){
   
 
   
 }
 
-gen.data()
+gen.data() # Now run the function defined above
 
 
 "
-# Livestock IDs 
-1 BULLS
-2 COWS
-3 STEERS
-4 HEIFERS
-5 MALE CALVES
-6 FEMALE CALVES
-
-
-# Sections
+Sections of livestock module. We will work specifically with sections 2 and 3
 1 - Hh roster
 2 - Livestock stock
 3 - Animal health
@@ -44,4 +38,12 @@ gen.data()
 7 - Draft power and dung
 8 - Other livestock products
 
+
+Livestock IDs (see questionnaires)
+1 BULLS
+2 COWS
+3 STEERS
+4 HEIFERS
+5 MALE CALVES
+6 FEMALE CALVES
 "
