dde2b506a244d3248497e2c1901a2712b71450a1
James Hawkins <jameshawkins702@gmail.com>
2023-11-01 10:37:03 +0100

    _

---
+++ main.R
@@ -0,0 +1,42 @@
+
+
+getwd()
+
+
+
+"
+# Livestock IDs 
+1 BULLS
+2 COWS
+3 STEERS
+4 HEIFERS
+5 MALE CALVES
+6 FEMALE CALVES
+
+"
+
+
+source('requirements.R')
+
+source('data.in.R')
+
+
+generate.data <- function(){
+  
+  source('section.1.R')
+  
+  source('plot.1.R')
+  
+  source('section.2.R')
+  
+  source('plot.2.R') 
+  
+}
+
+generate.data()
+
+
+plot.1
+plot.2
+plot.3
+plot.4
---
+++ requirements.R
@@ -0,0 +1,7 @@
+
+
+library('ggplot2')
+  
+`%+=%` <<- function(e1,e2) eval.parent(substitute(e1 <- e1 + e2))
+  
+
